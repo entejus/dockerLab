a4f782a0dc36f88354c60b2fa212791f586ccec5
entejus <38691478+entejus@users.noreply.github.com>
2018-11-30 18:27:01 +0100

    Update DockerMySQL.java

--- DockerMySQL.java
+++ DockerMySQL.java
@@ -2,7 +2,7 @@ import java.sql.*;
 
 public class DockerMySQL {
    static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";  
-   static final String DB_URL = "jdbc:mysql://10.0.10.3:3306/database";
+   static final String DB_URL = "jdbc:mysql://localhost:3306/database";
 
    static final String USER = "jmielniczuk";
    static final String PASS = "haslo";
