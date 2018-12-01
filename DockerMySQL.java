25ecc4021a848af7388cc4760d567d6b9bf8cfe3
entejus <38691478+entejus@users.noreply.github.com>
2018-12-01 09:01:06 +0100

    Update DockerMySQL.java

--- DockerMySQL.java
+++ DockerMySQL.java
@@ -4,9 +4,12 @@ import java.io.Console;
 
 public class DockerMySQL {
    static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";  
+   
+   //adres na jaki kontener łączy się z bazą
    static final String DB_URL = "jdbc:mysql://10.0.10.3:3306/database";
-
+   //nazwa użytkownika
    static final String USER = "jmielniczuk";
+   //hasło użytkownika
    static final String PASS = "haslo";
    
    public static void main(String[] args) {
@@ -18,6 +21,7 @@ public class DockerMySQL {
       System.out.println("Connecting to database...");
       Boolean connect = false;
     
+      //Pętla while wykonująca połączenie z bazą do momentu jego ustanowienia
       while(!connect)
       {
          try {
@@ -29,20 +33,29 @@ public class DockerMySQL {
             Thread.sleep(1000);
          }
       }
+      
       stmt = conn.createStatement();
       String dropTable,createTable,initInsert,insert,sql;
       
+      //Usunięcie tabeli jeśli istnieje
+      System.out.println("Droping table...");
       dropTable = "DROP TABLE IF EXISTS Osoby";
       stmt.executeUpdate(dropTable);
       
+      //Tworzenie tabeli Osoby z 3 polami
+      System.out.println("Creating table...");
       createTable = "CREATE TABLE Osoby (IdOsoby int, imie varchar(255), nazwisko varchar(255));";
       stmt.executeUpdate(createTable);
      
+      //Wypełnianie tabeli 3 rekordami
+      System.out.println("Filling table...");
       initInsert = "INSERT INTO Osoby (IdOsoby, imie, nazwisko) VALUES (1,'Jan','Kowalski')"+
                     ",(2,'Izabela','Kowal'),(3,'Krzysztof','Jarzyna');";
       stmt.executeUpdate(initInsert);
      
+      //Zapytanie SQL do wyszukania wszystkich rekordów w tabeli Osoby
       sql = "SELECT * FROM Osoby";
+      //Pierwsza część zapytania INSERT
       insert = "INSERT INTO Osoby (IdOsoby, imie, nazwisko) VALUES";
       
       Scanner input = new Scanner(System.in);
@@ -51,8 +64,7 @@ public class DockerMySQL {
       String imie,nazwisko;
            
       Boolean exit = false;
-     
-      
+    
       while(!exit) { 
       System.out.println("Wybierz jedną z opcji(wprowadź odpowiednią cyfrę):");
       System.out.println("[1] Wyświetl zawartość bazy");
@@ -60,16 +72,17 @@ public class DockerMySQL {
       System.out.println("[3] Wyjdź");
       
       int option = input.nextInt();
-         
+      
       switch(option) {
          case 1:   
+            //Wykonanie zapytania SELECT znajdującego się w zmiennej sql
             ResultSet rs = stmt.executeQuery(sql);
                while(rs.next()){
                   System.out.println("ID: " + rs.getInt(1)+", Imie: " + rs.getString(2)+", Nazwisko: " + rs.getString(3));
                }
             rs.close();
             break;
-         case 2:
+         case 2:            
             System.out.println("Podaj IdOsoby:");
             id = input.nextInt();
             System.out.println("Podaj imie:");
@@ -77,10 +90,13 @@ public class DockerMySQL {
             System.out.println("Podaj nazwisko:");
             nazwisko = input.next();
             
+            //Uzupełnienie zapytania na podstawie wprowadzonych danych
             insert+=" ("+id+",'"+imie+"','"+nazwisko+"');";
+            //Wprowadzenie danych do bazy
             stmt.executeUpdate(insert);
             break;
          case 3:
+            //Wyjście z pętli while
             exit = true;
             break;
          default:
